3d7e2710f434abb390df6321578e28c9b688b926
Kalaivani Natarajan <kalaivani.natarajan@softwire.com>
2025-01-22 16:07:06 +0000

    added post code and dynamic stop points

--- BusBoard.js
+++ BusBoard.js
@@ -3,29 +3,35 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const readline = require('readline-sync');
 
-const getStopCode = () => {
-    console.log('Please enter stop code');
-    let stopCode = readline.prompt();
-
-    return stopCode;
-};
-
 // (The stop code is 490008660N).
 
 const api_key = process.env.API_KEY
 
-const fetchTfl = async (api_key) => {
-    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${getStopCode()}/Arrivals?api=${api_key}`);
-    
+const fetchTfl = async (stopCode,api_key) => {
+    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals?api=${api_key}`);
+    return response;
+}
+
+const fetchTflStopPoints = async (postCodeResponse,api_key) => {
+    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${postCodeResponse.result.latitude}&lon=${postCodeResponse.result.longitude}&stopTypes=NaptanPublicBusCoachTram&modes=bus`);
+    return response;
+}
+
+const fetchPostCode = async (postCode) => {
+    const response = await fetch(`https://api.postcodes.io/postcodes/${postCode}`);    
     return response;
 }
 
 const printHeading = () => {
+    console.log("============================================================");
     console.log('Bus' + "\t" + 'Time' + "\t" + 'Destination' + "\t" + 'Route'); 
+    console.log("============================================================");
 }
 
-let response = await fetchTfl(api_key);
-const data = await response.json();
+const getPostCode=() => {
+    console.log("Please enter the post code :");
+    return readline.prompt();
+}
 
 const convertToMinutes = (seconds) => {
     const minutes = Math.floor(seconds / 60);
@@ -34,7 +40,7 @@ const convertToMinutes = (seconds) => {
 };
 
 
-const getDataFromResponse = () => {
+const getDataFromResponse = (data) => {
     const arr = [];
 
     data.forEach(element => {
@@ -56,18 +62,49 @@ const getDataFromResponse = () => {
 
 
 
-const printData = () => {
-    const data = getDataFromResponse();
+const printData = (data) => {
+    const output = getDataFromResponse(data);
 
     printHeading();
-    data.forEach(element => {
+    output.forEach(element => {
         console.log(element.lineId + "\t" + element.timeToStation + "\t" + element.destinationName + "\t" + element.towards)  
     })
 }
 
-printData()
+const getStopPoints = (stopPoints) => {
+    const arrStopPoints = [];
+    stopPoints.stopPoints.forEach(element => {
+        const obj = {
+            naptanId: element.naptanId,
+            distance: element.distance
+        };
+        arrStopPoints.push(obj);
 
-// console.log(getDataFromResponse())
+    });
+
+    arrStopPoints.sort((a,b) => a.distance - b.distance);
+    return arrStopPoints;
+}
+
+
+// Starting point
+const postCode = getPostCode();
+const postCodeResponse = await fetchPostCode(postCode);
+const postCodeData = await postCodeResponse.json();
+
+// GEt stop points from TFL
+const tflStopPointsResponse = await fetchTflStopPoints(postCodeData,api_key);
+const stopPoints = await tflStopPointsResponse.json();
+
+const arrStopPoints = getStopPoints(stopPoints);
+
+const tflResponse = await fetchTfl(arrStopPoints[0].naptanId,api_key);
+const data1 = await tflResponse.json();
+printData(data1);
+
+let response = await fetchTfl(arrStopPoints[1].naptanId,api_key);
+const data2 = await response.json();
+printData(data2);
 
 
 
